2844f801532160abc1c950ec02cfdad07a13b62e
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-06-13 16:43:10 +0300

    Print codeship environment variables

--- build.py
+++ build.py
@@ -1,7 +1,16 @@
+import os
+
+
 def build(*args, **kwargs):
     print('Building:')
     print('Args are:', args)
     print('Kwargs are:', kwargs)
+    variables = ['CI', 'CI_BUILD_NUMBER', 'CI_BUILD_URL', 'CI_PULL_REQUEST',
+                 'CI_BRANCH', 'CI_COMMIT_ID', 'CI_COMMITTER_NAME',
+                 'CI_COMMITTER_EMAIL', 'CI_COMMITTER_USERNAME', 'CI_MESSAGE',
+                 'CI_NAME']
+    for variable in variables:
+        print(os.environ[variable])
 
 
 if __name__ == '__main__':
