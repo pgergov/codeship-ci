2f7943de8e1bc56b79b95eed05753c8a90973384
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-06-13 20:24:42 +0300

    Add s3 upload

--- build.py
+++ build.py
@@ -1,11 +1,18 @@
 import os
 import boto3
+import shutil
+
+
+BRANCH_TO_FOLDER_MAPPING = {
+    'master': 'staging',
+    'develop': 'dev'
+}
 
 
 def build(*args, **kwargs):
     print('Building:')
-    print('Args are:', args)
-    print('Kwargs are:', kwargs)
+    print('Args are: ', args)
+    print('Kwargs are: ', kwargs)
     variables = ['CI', 'CI_BUILD_NUMBER', 'CI_BUILD_URL', 'CI_PULL_REQUEST',
                  'CI_BRANCH', 'CI_COMMIT_ID', 'CI_COMMITTER_NAME',
                  'CI_COMMITTER_EMAIL', 'CI_COMMITTER_USERNAME', 'CI_MESSAGE',
@@ -15,13 +22,27 @@ def build(*args, **kwargs):
 
     public_key = os.environ['AWS_ACCESS_KEY_ID']
     private_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    print(public_key)
-    print(private_key)
+    bucket = os.environ['AWS_STORAGE_BUCKET_NAME']
+
     s3 = boto3.client('s3',
                       aws_access_key_id=public_key,
                       aws_secret_access_key=private_key)
-    response = s3.list_buckets()
-    print(response)
+    print('Buckets response: ', s3.list_buckets())
+
+    # handle production manually
+    branch = os.environ.get('CI_BRANCH')
+    s3_container_folder = BRANCH_TO_FOLDER_MAPPING.get(branch, branch)
+    s3_build_folder = '1.2.0'
+
+    build_path = 'ui/build'
+    build_zip = shutil.make_archive(s3_build_folder, 'zip', build_path)
+
+    with open(build_zip, 'rb') as zip_data:
+        # make the file private
+        s3.upload_fileobj(zip_data,
+                          bucket,
+                          "{}/{}".format(s3_container_folder, s3_build_folder))
+    os.remove(build_zip)
 
 
 if __name__ == '__main__':
